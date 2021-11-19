ce03c0ace56e164969a943a317ac8cba5c73f72c
aleksandr.shleyko <aleksandr.shleyko@iqoption.com>
2021-11-19 19:48:28 +0300

    lection 1.12 - add test

---
+++ sandbox/src/test/java/ru/sandbox/SquareTests.java
@@ -0,0 +1,14 @@
+package ru.sandbox;
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class SquareTests {
+
+    @Test
+    public void testSquareMethod() {
+        Square square = new Square(5);
+        Assert.assertEquals(square.squareMethod(), 25);
+    }
+
+}
