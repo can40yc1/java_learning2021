bdd75a56e818208986858ffad698d047b54c4e8d
aleksandr.shleyko <aleksandr.shleyko@iqoption.com>
2021-11-19 17:49:57 +0300

    lection 1.5

--- sandbox/src/main/java/ru/sandbox/MyHomework1.java
+++ sandbox/src/main/java/ru/sandbox/MyHomework1.java
@@ -3,10 +3,11 @@ package ru.sandbox;
 public class MyHomework1 {
 
     public static void main(String[] args) {
-        System.out.println("Задание №1");
+        String what = "Задание";
+        System.out.println(what + " №1");
 
-        int size = 6;
-        int square = (int) Math.pow(size,2);
+        double size = 6;
+        double square = Math.pow(size, 2);
         System.out.println("Площадь квадрата со стороной " + size + " = " + square);
     }
 
