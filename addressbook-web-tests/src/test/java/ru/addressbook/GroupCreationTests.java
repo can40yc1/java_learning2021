9dfedd7a78964099068e3068e75989541c06f705
aleksandr.shleyko <aleksandr.shleyko@iqoption.com>
2021-11-26 19:49:46 +0300

    clearing first test

--- addressbook-web-tests/src/test/java/ru/addressbook/GroupCreationTests.java
+++ addressbook-web-tests/src/test/java/ru/addressbook/GroupCreationTests.java
@@ -1,12 +1,13 @@
 package ru.addressbook;
 
-import org.openqa.selenium.*;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.testng.annotations.*;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
 
 import java.util.concurrent.TimeUnit;
-
-import static org.testng.Assert.fail;
 
 public class GroupCreationTests {
     private WebDriver wd;
@@ -21,7 +22,6 @@ public class GroupCreationTests {
 
     private void login(String username, String password) {
         wd.get("http://localhost/addressbook/index.php");
-        wd.findElement(By.name("user")).click();
         wd.findElement(By.name("user")).clear();
         wd.findElement(By.name("user")).sendKeys(username);
         wd.findElement(By.name("pass")).clear();
@@ -47,7 +47,6 @@ public class GroupCreationTests {
     }
 
     private void fillGroupForm(GroupData groupData) {
-        wd.findElement(By.name("group_name")).click();
         wd.findElement(By.name("group_name")).clear();
         wd.findElement(By.name("group_name")).sendKeys(groupData.getName());
         wd.findElement(By.name("group_header")).clear();
@@ -68,24 +67,6 @@ public class GroupCreationTests {
     public void tearDown() throws Exception {
         wd.findElement(By.linkText("Logout")).click();
         wd.quit();
-    }
-
-    private boolean isElementPresent(By by) {
-        try {
-            wd.findElement(by);
-            return true;
-        } catch (NoSuchElementException e) {
-            return false;
-        }
-    }
-
-    private boolean isAlertPresent() {
-        try {
-            wd.switchTo().alert();
-            return true;
-        } catch (NoAlertPresentException e) {
-            return false;
-        }
     }
 
 }
