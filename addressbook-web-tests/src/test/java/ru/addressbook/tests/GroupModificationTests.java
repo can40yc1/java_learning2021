57946d30f062f6bc520f103afbc056e6b87c7d88
aleksandr.shleyko <aleksandr.shleyko@iqoption.com>
2021-12-03 17:53:14 +0300

    Lection 3.10 - check existing group

--- addressbook-web-tests/src/test/java/ru/addressbook/tests/GroupModificationTests.java
+++ addressbook-web-tests/src/test/java/ru/addressbook/tests/GroupModificationTests.java
@@ -8,6 +8,9 @@ public class GroupModificationTests extends TestBase {
     @Test
     public void testGroupModofocation() {
         app.getNavigationHelper().goToGroupPage();
+        if (! app.getGroupHelper().isThereAGroup()) {
+            app.getGroupHelper().createGroup(new GroupData("test1", null, null));
+        }
         app.getGroupHelper().selectGroup();
         app.getGroupHelper().initGroupModification();
         app.getGroupHelper().fillGroupForm(new GroupData("test10", "test20", "test30"));
