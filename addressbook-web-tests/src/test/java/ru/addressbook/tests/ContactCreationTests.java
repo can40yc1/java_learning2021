c80cdecf857c8b5e108de240545d15c679410c49
aleksandr.shleyko <aleksandr.shleyko@iqoption.com>
2021-12-08 18:59:30 +0300

    ContactCreationTest fix

--- addressbook-web-tests/src/test/java/ru/addressbook/tests/ContactCreationTests.java
+++ addressbook-web-tests/src/test/java/ru/addressbook/tests/ContactCreationTests.java
@@ -7,11 +7,8 @@ public class ContactCreationTests extends TestBase {
 
     @Test
     public void testContactCreation() {
-        app.getContactHelper().initContactCreation();
-        app.getContactHelper().fillContactForm(new ContactData("first", "middle", "last",
-                "nick", "mister", "microsoft", "NY, Statue of Liberty", "test1"), true);
-        app.getContactHelper().submitContactCreation();
-        app.getContactHelper().returnToHome();
-    }
 
+        app.getContactHelper().createContact(new ContactData("first", "middle", "last",
+                "nick", "mister", "microsoft", "NY, Statue of Liberty", "test1"));
+    }
 }
