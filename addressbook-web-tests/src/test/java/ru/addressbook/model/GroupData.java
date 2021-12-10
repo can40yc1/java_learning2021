b872db2715d25e0d791160d1b6203ec245f15f97
aleksandr.shleyko <aleksandr.shleyko@iqoption.com>
2021-12-10 19:23:57 +0300

    Lection 4.8 - check groups list after creation

--- addressbook-web-tests/src/test/java/ru/addressbook/model/GroupData.java
+++ addressbook-web-tests/src/test/java/ru/addressbook/model/GroupData.java
@@ -6,9 +6,9 @@ public class GroupData {
     private final String name;
     private final String header;
     private final String footer;
-    private final String id;
+    private int id;
 
-    public GroupData(String name, String header, String footer, String id) {
+    public GroupData(String name, String header, String footer, int id) {
         this.name = name;
         this.header = header;
         this.footer = footer;
@@ -19,10 +19,15 @@ public class GroupData {
         this.name = name;
         this.header = header;
         this.footer = footer;
-        this.id = null;
+        this.id = 0;
     }
 
-    public String getId() {
+    public GroupData setId(int id) {
+        this.id = id;
+        return this;
+    }
+
+    public int getId() {
         return id;
     }
 
@@ -51,7 +56,7 @@ public class GroupData {
         if (this == o) return true;
         if (o == null || getClass() != o.getClass()) return false;
         GroupData groupData = (GroupData) o;
-        return Objects.equals(name, groupData.name) && Objects.equals(id, groupData.id);
+        return id == groupData.id && Objects.equals(name, groupData.name);
     }
 
     @Override
--- addressbook-web-tests/src/test/java/ru/addressbook/tests/GroupCreationTests.java
+++ addressbook-web-tests/src/test/java/ru/addressbook/tests/GroupCreationTests.java
@@ -4,6 +4,7 @@ import org.testng.Assert;
 import org.testng.annotations.Test;
 import ru.addressbook.model.GroupData;
 
+import java.util.HashSet;
 import java.util.List;
 
 public class GroupCreationTests extends TestBase {
@@ -13,11 +14,20 @@ public class GroupCreationTests extends TestBase {
         app.getNavigationHelper().goToGroupPage();
         List<GroupData> before = app.getGroupHelper().getGroupList();
         app.getGroupHelper().initGroupCreation();
-        app.getGroupHelper().fillGroupForm(new GroupData("test1", null, null));
+        GroupData group = new GroupData("test1", null, null);
+        app.getGroupHelper().fillGroupForm(group);
         app.getGroupHelper().submitGroupCreation();
         app.getGroupHelper().returnToGroupPage();
         List<GroupData> after = app.getGroupHelper().getGroupList();
         Assert.assertEquals(after.size(), before.size() + 1);
+        int max = 0;
+        for (GroupData g : after) {
+            if (g.getId() > max) {
+                max = g.getId();
+            }
+        }
+        before.add(group.setId(max));
+        Assert.assertEquals(new HashSet<Object>(after), new HashSet<Object>(before));
     }
 
 }
