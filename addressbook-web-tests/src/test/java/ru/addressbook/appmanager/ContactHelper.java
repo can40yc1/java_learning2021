598ac9b45264c6a122ea10555e3bdb7116132c30
aleksandr.shleyko <aleksandr.shleyko@iqoption.com>
2021-12-17 20:36:58 +0300

    Lection 5.10 - split strings

--- addressbook-web-tests/src/test/java/ru/addressbook/appmanager/ContactHelper.java
+++ addressbook-web-tests/src/test/java/ru/addressbook/appmanager/ContactHelper.java
@@ -100,8 +100,15 @@ public class ContactHelper extends HelperBase {
         for (WebElement element : elements) {
             String firstName = element.findElement(By.xpath("./td[3]")).getText();
             String lastName = element.findElement(By.xpath("./td[2]")).getText();
+            String[] phones = element.findElement(By.xpath("./td[6]")).getText().split("\n");
             int id = Integer.parseInt(element.findElement(By.tagName("input")).getAttribute("id"));
-            ContactData contact = new ContactData().withId(id).withFirstname(firstName).withLastname(lastName);
+            ContactData contact = new ContactData()
+                    .withId(id)
+                    .withFirstname(firstName)
+                    .withLastname(lastName)
+                    .withHomePhone(phones[0])
+                    .withMobilePhone(phones[1])
+                    .withWorkPhone(phones[2]);
             contacts.add(contact);
         }
         return contacts;
--- addressbook-web-tests/src/test/java/ru/addressbook/tests/ContactPhoneTests.java
+++ addressbook-web-tests/src/test/java/ru/addressbook/tests/ContactPhoneTests.java
@@ -1,7 +1,12 @@
 package ru.addressbook.tests;
 
+import org.hamcrest.CoreMatchers;
+import org.hamcrest.MatcherAssert;
 import org.testng.annotations.Test;
 import ru.addressbook.model.ContactData;
+
+import static org.hamcrest.CoreMatchers.*;
+import static org.hamcrest.MatcherAssert.*;
 
 public class ContactPhoneTests extends TestBase {
 
@@ -10,5 +15,13 @@ public class ContactPhoneTests extends TestBase {
         app.goTo().homePage();
         ContactData contact = app.contactSteps().getAll().iterator().next();
         ContactData contactInfoFromEditForm = app.contactSteps().infoFromEditForm(contact);
+
+        assertThat(contact.getHomePhone(), equalTo(cleaned(contactInfoFromEditForm.getHomePhone())));
+        assertThat(contact.getMobilePhone(), equalTo(cleaned(contactInfoFromEditForm.getMobilePhone())));
+        assertThat(contact.getWorkPhone(), equalTo(cleaned(contactInfoFromEditForm.getWorkPhone())));
+    }
+
+    public String cleaned(String phone) {
+        return phone.replaceAll("\\s", "").replaceAll("[-()]", "");
     }
 }
