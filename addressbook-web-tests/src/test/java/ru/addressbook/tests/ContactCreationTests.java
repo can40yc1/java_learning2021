4594fdedf7f9d1c05a51925fe5d4d25a41782800
aleksandr.shleyko <aleksandr.shleyko@iqoption.com>
2022-01-16 18:28:05 +0300

    Homework 13 - use generated data in contact creation test

--- addressbook-web-tests/src/test/java/ru/addressbook/tests/ContactCreationTests.java
+++ addressbook-web-tests/src/test/java/ru/addressbook/tests/ContactCreationTests.java
@@ -1,31 +1,46 @@
 package ru.addressbook.tests;
 
+import com.google.gson.Gson;
+import org.openqa.selenium.json.TypeToken;
+import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 import ru.addressbook.model.ContactData;
 import ru.addressbook.model.Contacts;
 
+import java.io.BufferedReader;
 import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.Iterator;
+import java.util.List;
+import java.util.stream.Collectors;
 
 import static org.hamcrest.CoreMatchers.equalTo;
 import static org.hamcrest.MatcherAssert.assertThat;
 
 public class ContactCreationTests extends TestBase {
 
-    @Test
-    public void testContactCreation() {
+    @DataProvider
+    public Iterator<Object[]> contactsFromJson() throws IOException {
+        BufferedReader reader = new BufferedReader(new FileReader(new File("src/test/resources/contacts.json")));
+        String json = "";
+        String line = reader.readLine();
+        while (line != null) {
+            json += line;
+            line = reader.readLine();
+        }
+        Gson gson = new Gson();
+        List<ContactData> contacts = gson.fromJson(json, new TypeToken<List<ContactData>>() {
+        }.getType());
+        return contacts.stream().map((g) -> new Object[]{g}).collect(Collectors.toList()).iterator();
+    }
+
+    @Test(dataProvider = "contactsFromJson")
+    public void testContactCreation(ContactData contact) {
 
         File photo = new File("src/test/resources/photo1.png");
         Contacts before = app.contactSteps().getAll();
-        ContactData contact = new ContactData()
-                .withFirstname("first")
-                .withMiddlename("middle")
-                .withLastname("last")
-                .withNickname("nick")
-                .withTitle("mister")
-                .withCompany("microsoft")
-                .withAddress("NY, Statue of Liberty")
-                .withGroup("test1")
-                .withPhoto(photo);
+        contact.withPhoto(photo);
 
         app.contactSteps().create(contact);
         Contacts after = app.contactSteps().getAll();
