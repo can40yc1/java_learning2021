a895f60925d7e9bd2aa01bbf806fdb7c40da0d05
aleksandr.shleyko <aleksandr.shleyko@iqoption.com>
2022-01-17 18:53:19 +0300

    Lection 7.5 - optional steps for UI

--- addressbook-web-tests/src/test/java/ru/addressbook/tests/TestBase.java
+++ addressbook-web-tests/src/test/java/ru/addressbook/tests/TestBase.java
@@ -8,10 +8,16 @@ import org.testng.annotations.AfterSuite;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.BeforeSuite;
 import ru.addressbook.appmanager.ApplicationManager;
+import ru.addressbook.model.GroupData;
+import ru.addressbook.model.Groups;
 
 import java.io.IOException;
 import java.lang.reflect.Method;
 import java.util.Arrays;
+import java.util.stream.Collectors;
+
+import static org.hamcrest.CoreMatchers.equalTo;
+import static org.hamcrest.MatcherAssert.assertThat;
 
 public class TestBase {
 
@@ -37,6 +43,18 @@ public class TestBase {
     @AfterMethod(alwaysRun = true)
     public void logTestStop(Method method, Object[] parameters) {
         logger.info("Stop test " + method.getName() + " with parameters " + Arrays.asList(parameters));
+    }
+
+    public void verifyGroupListInUI() {
+        if (Boolean.getBoolean("verifyUI")) {
+            Groups dbGroups = app.db().groups();
+            Groups uiGroups = app.groupSteps().getAll();
+            assertThat(uiGroups, equalTo(dbGroups.stream()
+                    .map((g) -> new GroupData()
+                            .withId(g.getId())
+                            .withName(g.getName()))
+                    .collect(Collectors.toSet())));
+        }
     }
 
 }
