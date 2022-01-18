0ef5e33c4cc4028daa03e0bbdc45416e2d63bced
aleksandr.shleyko <aleksandr.shleyko@iqoption.com>
2022-01-18 15:29:55 +0300

    Lection 8.2 - mantisbt test module init

---
+++ mantis-tests/src/test/java/ru/mantis/tests/TestBase.java
@@ -0,0 +1,24 @@
+package ru.mantis.tests;
+
+import org.openqa.selenium.remote.Browser;
+import org.testng.annotations.AfterSuite;
+import org.testng.annotations.BeforeSuite;
+import ru.mantis.appmanager.ApplicationManager;
+
+import java.io.IOException;
+
+public class TestBase {
+
+    protected static final ApplicationManager app = new ApplicationManager(System.getProperty("browser", Browser.CHROME.browserName()));
+
+    @BeforeSuite(alwaysRun = true)
+    public void setUp() throws IOException {
+        app.init();
+    }
+
+    @AfterSuite(alwaysRun = true)
+    public void tearDown() {
+        app.stop();
+    }
+
+}
