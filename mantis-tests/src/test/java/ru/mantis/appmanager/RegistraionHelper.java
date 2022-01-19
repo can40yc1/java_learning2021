5dd19da8c21f3ec40d9951b6c53754eb71063f0b
aleksandr.shleyko <aleksandr.shleyko@iqoption.com>
2022-01-19 14:56:43 +0300

    Lection 8.6 - local mail server

--- mantis-tests/src/test/java/ru/mantis/appmanager/RegistraionHelper.java
+++ mantis-tests/src/test/java/ru/mantis/appmanager/RegistraionHelper.java
@@ -1,17 +1,25 @@
 package ru.mantis.appmanager;
 
-import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.By;
 
-public class RegistraionHelper {
-    private final ApplicationManager app;
-    private WebDriver wd;
+public class RegistraionHelper extends HelperBase {
 
     public RegistraionHelper(ApplicationManager app) {
-        this.app = app;
-        wd = app.getDriver();
+        super(app);
     }
 
     public void start(String username, String email) {
         wd.get(app.getProperty("web.baseUrl") + "/signup_page.php");
+        type(By.name("username"), username);
+        type(By.name("email"), email);
+        click(By.cssSelector("input[value='Зарегистрироваться']"));
+
+    }
+
+    public void finish(String confirmationLink, String password) {
+        wd.get(confirmationLink);
+        type(By.name("password"), password);
+        type(By.name("password_confirm"), password);
+        click(By.cssSelector("input[value='Изменить учетную запись']"));
     }
 }
--- mantis-tests/src/test/resources/config_inc.php
+++ mantis-tests/src/test/resources/config_inc.php
@@ -6,3 +6,5 @@ $g_db_username = 'root';
 $g_db_password = '';
 $g_crypto_master_salt = 'qpRONmyqBclCnzkTUWKlpXdb73KW6FNXSVnUd0a7COQ=';
 $g_signup_use_captcha = OFF;
+$g_phpMailer_method = PHPMAILER_METHOD_SMTP;
+$g_smtp_host = 'localhost';
