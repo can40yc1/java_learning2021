2a03a8de21f78519d6505a5ab79ae2e2b1e2db7f
aleksandr.shleyko <aleksandr.shleyko@iqoption.com>
2022-01-21 18:06:49 +0300

    Lection 9.5 - restassured lib test

---
+++ rest-example/src/test/java/ru/rest/RestAssuredTests.java
@@ -0,0 +1,53 @@
+package ru.rest;
+
+import com.google.gson.Gson;
+import com.google.gson.JsonElement;
+import com.google.gson.reflect.TypeToken;
+import com.jayway.restassured.RestAssured;
+import org.apache.http.client.fluent.Executor;
+import org.apache.http.client.fluent.Request;
+import org.apache.http.message.BasicNameValuePair;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+
+import java.io.IOException;
+import java.util.Set;
+
+import static com.google.gson.JsonParser.parseString;
+import static org.testng.Assert.assertEquals;
+
+public class RestAssuredTests {
+
+    @BeforeClass
+    public void init() {
+        RestAssured.authentication = RestAssured.basic("288f44776e7bec4bf44fdfeb1e646490", "");
+    }
+
+    @Test
+    public void testCreateIssue() throws IOException {
+        Set<Issue> oldIssues = getIssues();
+        Issue newIssue = new Issue().withSubject("test test").withDescription("my test");
+        int issueId = createIssue(newIssue);
+        Set<Issue> newIssues = getIssues();
+        oldIssues.add(newIssue.withId(issueId));
+        assertEquals(newIssues, oldIssues);
+
+    }
+
+    private Set<Issue> getIssues() throws IOException {
+        String json = RestAssured.get("https://bugify.stqa.ru/api/issues.json").asString();
+        JsonElement parsed = parseString(json);
+        JsonElement issues = parsed.getAsJsonObject().get("issues");
+        return new Gson().fromJson(issues, new TypeToken<Set<Issue>>() {
+        }.getType());
+    }
+
+    private int createIssue(Issue issue) throws IOException {
+        String json = RestAssured.given()
+                .parameter("subject", issue.getSubject())
+                .parameter("description", issue.getDescription())
+                .post("https://bugify.stqa.ru/api/issues.json").asString();
+        JsonElement parsed = parseString(json);
+        return parsed.getAsJsonObject().get("issue_id").getAsInt();
+    }
+}
